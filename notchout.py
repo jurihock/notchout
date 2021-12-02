bebdf6c39f8549f1a3342af81bc080f1bbec1d03
Jürgen Hock <juergen.hock@jurihock.de>
2021-12-03 00:03:57 +0100

    Create basic script

---
+++ notchout.py
@@ -0,0 +1,73 @@
+import AppKit
+
+import PIL
+import PIL.Image
+import PIL.ImageColor
+
+import click
+import numpy as np
+import os
+import sys
+
+
+def screensize(screen=0, width=None, height=None):
+
+    screens = AppKit.NSScreen.screens()
+
+    screen = sorted((0, screen, len(screens) - 1))[1]
+    screen = screens[screen]
+
+    frame = screen.frame()
+    scale = screen.backingScaleFactor()
+
+    width = int(width or frame.size.width * scale)
+    height = int(height or frame.size.height * scale)
+
+    return width, height
+
+
+def rgbcolor(hexcolor):
+
+    return PIL.ImageColor.getcolor(hexcolor, 'RGB')
+
+
+def save(image, path):
+
+    PIL.Image.fromarray(image, 'RGB').save(path)
+
+
+@click.command(help=None)
+@click.argument('path', default='~/Pictures/notchout.png')
+@click.option('-s', '--screen', default=1, help='Screen index for auto image size detection. (1)')
+@click.option('-w', '--width', default=0,  help='Image width.  (auto)')
+@click.option('-h', '--height', default=0, help='Image height. (auto)')
+@click.option('-n', '--notch', default=74, help='Notch height. (74)')
+@click.option('-c', '--color', default='#191919', help='Color code.   (#191919)')
+def notchout(path, screen, width, height, notch, color):
+
+    try:
+
+        path = os.path.abspath(os.path.expanduser(path))
+        screen -= 1
+        width, height = screensize(screen=screen, width=width, height=height)
+        color = rgbcolor(color)
+
+        click.echo(f'Creating image of size {width} x {height}')
+        image = np.zeros((height, width, 3), dtype=np.uint8)
+
+        click.echo(f'Filling image with color {color}')
+        click.echo(f'Masking notch of height {notch}')
+        image[notch:, :] = color
+
+        click.echo(f'Writing image to {path}')
+        save(image, path)
+
+    except Exception as exception:
+
+        click.echo(str(exception), err=True)
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+
+    notchout()
